496c22a2599087611c525080f5713ddbca5e497c
philFernandez <norcal618@gmail.com>
2019-05-05 16:48:09 -0700

    move database writes to method

--- Lost & Found/src/DeviceDatabase.java
+++ Lost & Found/src/DeviceDatabase.java
@@ -5,43 +5,38 @@ class DeviceDatabase {
     private static DeviceDatabase instance;
     private static File file = new File("./deviceDatabase.dat");
     private static ArrayList<String> loadDatabaseIntoMemory;
-    // private static Scanner input;
+    private static Scanner input;
 
     private DeviceDatabase() {
         // private constructor
     }
 
     public synchronized static DeviceDatabase getInstance() {
-        Scanner input;
+        // Scanner input;
         if(instance == null) {
             loadDatabaseIntoMemory = new ArrayList<String>();
-
-            try {
-                input = new Scanner(file);
-                while (input.hasNextLine()) {
-                    loadDatabaseIntoMemory.add(input.nextLine());
-                }
-            } catch (Exception e) {
-                
-            }
-
+            updateDatabase();
             instance = new DeviceDatabase();
             System.out.println("DeviceDatabase created....");
         }
         else {
             loadDatabaseIntoMemory.clear();
-            try {
-                input = new Scanner(file);
-                while (input.hasNextLine()) {
-                    loadDatabaseIntoMemory.add(input.nextLine());
-                }
-            } catch (Exception e) {
-                
-            }
-
+            updateDatabase();
             System.out.println("Return existing DeviceDatabase....");
         }
+        input.close();
         return instance;
+    }
+
+    public static void updateDatabase() {
+        try {
+            input = new Scanner(file);
+            while (input.hasNextLine()) {
+                loadDatabaseIntoMemory.add(input.nextLine());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static ArrayList<String> readDatabase() {
